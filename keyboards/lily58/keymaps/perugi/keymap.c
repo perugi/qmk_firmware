fd684954837ce4dde90dfe29e2d461026a810b92
Dominik Perusko <dominik.perusko@gmail.com>
2022-10-28 12:28:22 +0200

    Implement new location for single-hand delete

--- keyboards/lily58/keymaps/perugi/keymap.c
+++ keyboards/lily58/keymaps/perugi/keymap.c
@@ -73,11 +73,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT(
-  KC_GRV,               KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,       KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
-  KC_TAB,               KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-  MT(MOD_LCTL, KC_ESC), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,       KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,              KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,       KC_LBRC,  KC_RBRC,  KC_N,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-                                         KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC,   KC_ENT,   MO(_RAISE), KC_BSPC, OSL(_SLO)
+  KC_GRV,               KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,       KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+  KC_TAB,               KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+  MT(MOD_LCTL, KC_ESC), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,       KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT,              KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,       KC_LBRC, KC_RBRC,  KC_N,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
+                                         KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC,  KC_ENT,   MO(_RAISE), KC_BSPC, OSL(_SLO)
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -85,20 +85,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   -  |   1  |   2  |   3  |   4  |   5  |-------.    ,-------|   6  |   7  |   8  |   9  |   0  |   =  |
+ * |E/LCTL|   1  |   2  |   3  |   4  |   5  |-------.    ,-------|   6  |   7  |   8  |   9  |   0  |   =  |
  * |------+------+------+------+------+------|   {   |    |    }  |------+------+------+------+------+------|
- * |   _  |   !  |   @  |   #  |   $  |   %  |-------|    |-------|   ^  |   &  | * / ,| ( / .| ) / :|   +  |
+ * |LShift|   !  |   @  |   #  |   $  |   %  |-------|    |-------|   ^  |   &  | * / ,| ( / .| ) / :|   +  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | DEL  | LAlt |LOWER | /Space  /       \Enter \  |RAISE |BackSP| SLO  |
+ *                   | LGUI | LAlt |LOWER | /  DEL  /       \Enter \  |RAISE |BackSP| SLO  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-  XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX,             XXXXXXX, XXXXXXX,
-  KC_F1,      KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,              KC_F10,              KC_F11,  KC_F12,
-  KC_MINS,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,               KC_9,                KC_0,    KC_EQL,
-  S(KC_MINS), S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), KC_LCBR, KC_RCBR, S(KC_6), S(KC_7), TD(TD_COMMA_LOWER), TD(TD_PERIOD_LOWER), TD(TD_COLON_LOWER), S(KC_EQL),
-                                KC_DEL,  _______, _______, _______, _______, _______, _______, _______
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX,             XXXXXXX, XXXXXXX,
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,              KC_F10,              KC_F11,  KC_F12,
+  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,               KC_9,                KC_0,    KC_EQL,
+  _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), KC_LCBR, KC_RCBR, S(KC_6), S(KC_7), TD(TD_COMMA_LOWER), TD(TD_PERIOD_LOWER), TD(TD_COLON_LOWER), S(KC_EQL),
+                             _______, _______, _______, KC_DEL,  _______, _______, _______, _______
 ),
 
 /* RAISE
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |E/LCTL|   !  |   ~  | PGDN | PGUP | H/E  |-------.    ,-------| Left | Down |  Up  |Right |      |      |
  * |------+------+------+------+------+------|   {   |    |    }  |------+------+------+------+------+------|
- * |LShift|      |  DEL |      |      | BEG  |-------|    |-------|     |      |      |      |Search|      |
+ * |LShift|      |  DEL |      |      | BEG  |-------|    |-------|      |      |      |      |Search|      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LGUI | LAlt |LOWER | /Space  /       \Enter \  |RAISE |BackSP| SLO  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
