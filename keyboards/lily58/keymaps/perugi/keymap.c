d7ab968785c1c09fda41eaaf2dbb082e1cd6a2eb
perugi <dominik.perusko@gmail.com>
2025-01-17 20:43:32 +0100

    Move esc to separate layer

--- keyboards/lily58/keymaps/perugi/keymap.c
+++ keyboards/lily58/keymaps/perugi/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
      * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |E/LCTL|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+     * | LCtl |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
      * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
      * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  -   |
      * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -72,11 +72,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [_QWERTY] = LAYOUT(
-        KC_GRAVE,             KC_1,    KC_2,       KC_3,    KC_4,    KC_5,                         KC_6,       KC_7,    KC_8,    KC_9,   KC_0,    KC_EQL,
-        KC_TAB,               KC_Q,    KC_W,       KC_E,    KC_R,    KC_T,                         KC_Y,       KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
-        MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,       KC_D,    KC_F,    KC_G,                         KC_H,       KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-        KC_LSFT,              KC_Z,    KC_X,       KC_C,    KC_V,    KC_B,       KC_LBRC, KC_RBRC, KC_N,       KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
-                                                   KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC,  KC_ENT,  MO(_RAISE), KC_BSPC, OSL(_SLO)
+        KC_GRAVE, KC_1,    KC_2,       KC_3,    KC_4,    KC_5,                         KC_6,       KC_7,    KC_8,    KC_9,   KC_0,    KC_EQL,
+        KC_TAB,   KC_Q,    KC_W,       KC_E,    KC_R,    KC_T,                         KC_Y,       KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
+        KC_LCTL   KC_A,    KC_S,       KC_D,    KC_F,    KC_G,                         KC_H,       KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+        KC_LSFT,  KC_Z,    KC_X,       KC_C,    KC_V,    KC_B,       KC_LBRC, KC_RBRC, KC_N,       KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
+                                       KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC,  KC_ENT,  MO(_RAISE), KC_BSPC, OSL(_SLO)
         ),
 
     /* LOWER
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
      * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |E/LCTL|   1  |   2  |   3  |   4  |   5  |-------.    ,-------|   6  |   7  |   8  |   9  |   0  |   =  |
+     * | Esc  |   1  |   2  |   3  |   4  |   5  |-------.    ,-------|   6  |   7  |   8  |   9  |   0  |   =  |
      * |------+------+------+------+------+------|   {   |    |    }  |------+------+------+------+------+------|
      * |LShift|   !  |   @  |   #  |   $  |   %  |-------|    |-------|   ^  |   &  | * / ,| ( / .|   )  |   +  |
      * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
         _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), KC_LCBR, KC_RCBR, S(KC_6), S(KC_7), TD(TD_COMMA_LOWER), TD(TD_PERIOD_LOWER), S(KC_0), S(KC_EQL),
                                    _______, _______, _______, KC_DEL,  _______, _______, _______, _______
         ),
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
      * |      |      |      | END  | Redo |      |                    |      | Undo |      |      |PrntSc|      |
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |E/LCTL|   !  |   ~  | PGDN | PGUP | E/H  |-------.    ,-------| Left | Down |  Up  |Right |      |      |
+     * | Esc  |   !  |   ~  | PGDN | PGUP | E/H  |-------.    ,-------| Left | Down |  Up  |Right |      |      |
      * |------+------+------+------+------+------|   {   |    |    }  |------+------+------+------+------+------|
      * |LShift|      |  DEL |      |      | BEG  |-------|    |-------|      |      |      |      |Search|      |
      * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -119,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_RAISE] = LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,     XXXXXXX, XXXXXXX,                           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX,   C(KC_RIGHT), C(KC_Y), XXXXXXX,                           XXXXXXX, C(KC_Z), XXXXXXX, XXXXXXX, KC_PRINT_SCREEN, XXXXXXX,
-        _______, S(KC_1), S(KC_GRV), KC_PGDN,     KC_PGUP, TD(TD_END_HOME),                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,         XXXXXXX,
+        KC_ESC,  S(KC_1), S(KC_GRV), KC_PGDN,     KC_PGUP, TD(TD_END_HOME),                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,         XXXXXXX,
         _______, XXXXXXX, KC_DEL,    XXXXXXX,     XXXXXXX, C(KC_LEFT),      KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, C(KC_F),         XXXXXXX,
                                      _______,     _______, _______,         _______, _______, _______, _______, _______
         ),
@@ -152,7 +152,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
      * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |E/LCTL|      |   š  |      |      |      |-------.    ,-------|      |      |      |      |      |   ć  |
+     * | Esc  |      |   š  |      |      |      |-------.    ,-------|      |      |      |      |      |   ć  |
      * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
      * |LShift|   ž  |      |   č  |   €  |      |-------|    |-------|      |      |      |      |      |      |
      * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -163,7 +163,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_SLO] = LAYOUT(
         XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, XXXXXXX,    UP(0, 1),   XXXXXXX,    XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, UP(cch, CCH),
+        KC_ESC,  XXXXXXX,    UP(0, 1),   XXXXXXX,    XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, UP(cch, CCH),
         _______, UP(zh, ZH), XXXXXXX,    UP(ch, CH), UM(EUR), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                          _______,    _______, _______, _______, _______, _______, _______, _______
     ),
